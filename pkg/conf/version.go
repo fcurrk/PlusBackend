788c8c77b8d20357a431b8e561e46f282f43d681
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-02-26 10:21:34 +0800

    Update version.go

--- pkg/conf/version.go
+++ pkg/conf/version.go
@@ -1,19 +1,19 @@
 package conf
 
 // plusVersion 增强版版本号
-const plusVersion = "+1.1"
+const plusVersion = "2402"
 
 // BackendVersion 当前后端版本号
-const BackendVersion = "3.8.3" + plusVersion
+const BackendVersion = "3.8.3." + plusVersion
 
 // KeyVersion 授权版本号
 const KeyVersion = "3.3.1"
 
 // RequiredDBVersion 与当前版本匹配的数据库版本
-const RequiredDBVersion = "3.8.1+1.0-plus"
+const RequiredDBVersion = "3.8.1.2402"
 
 // RequiredStaticVersion 与当前版本匹配的静态资源版本
-const RequiredStaticVersion = "3.8.3" + plusVersion
+const RequiredStaticVersion = "3.8.3." + plusVersion
 
 // IsPlus 是否为Plus版本
 const IsPlus = "true"
