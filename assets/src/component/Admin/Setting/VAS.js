0e804bcebfa6803423a0ad036d09879d2f093693
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-05-23 16:38:44 +0800

    Update VAS.js

--- assets/src/component/Admin/Setting/VAS.js
+++ assets/src/component/Admin/Setting/VAS.js
@@ -1137,6 +1137,7 @@ export default function VAS() {
                                                         </>
                                                     )}
                                                     {row.ProductID !== 0 &&
+                                                         ProductID !== 9 &&
                                                         !product[
                                                             row.ProductID
                                                         ] &&
