f20602c3e711ac3d344d4674ff49bda4a8c58f97
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-01-19 11:33:10 +0800

    Update 3.8.3.2501
    fix: add oss callbackSNI support.
    update build.yml

--- pkg/conf/version.go
+++ pkg/conf/version.go
@@ -1,7 +1,7 @@
 package conf
 
 // plusVersion 增强版版本号
-const plusVersion = "2407"
+const plusVersion = "2501"
 
 // BackendVersion 当前后端版本号
 const BackendVersion = "3.8.3." + plusVersion
@@ -10,7 +10,7 @@ const BackendVersion = "3.8.3." + plusVersion
 const KeyVersion = "3.8.3"
 
 // RequiredDBVersion 与当前版本匹配的数据库版本
-const RequiredDBVersion = "3.8.3.2407"
+const RequiredDBVersion = "3.8.3.2501"
 
 // RequiredStaticVersion 与当前版本匹配的静态资源版本
 const RequiredStaticVersion = "3.8.3." + plusVersion
@@ -19,4 +19,4 @@ const RequiredStaticVersion = "3.8.3." + plusVersion
 const IsPlus = "true"
 
 // LastCommit 最后commit id
-const LastCommit = "b323bc5"
+const LastCommit = "9999f6e"
